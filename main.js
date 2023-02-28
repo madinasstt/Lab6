a59cf07efb59bebdb433c982665b0abff83f4e23
madinasstt <madina.seisimbinova@nu.edu.kz>
2023-02-28 16:33:06 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function perimeterRectangle(a, b) {
+	let perimeter = 2 * (a + b);
+	return perimeter;
+}
+let a = 10;
+let b = 9;
+console.log("Perimeter = " + perimeterRectangle(a, b));
+
+
+let pi = 3.14159265358979323846;
+function findArea(r) {
+	return (pi * r * r);
+}
+let r, Area;
+    r = 5;
+	Area = findArea(r);
+	console.log("Area of Circle is: " + Area);
+
+
+
+console.log( 'Hello World\n'.repeat(3) )
+	  
